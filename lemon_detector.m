3832d965eda4af21295e10186708e8d9a0adecda
alonsovega <vegaa@umich.edu>
2020-09-25 16:34:51 -0400

    detected

--- lemon_detector.m
+++ lemon_detector.m
@@ -1,9 +1,9 @@
 
 
 
-%%
+%% Get Blob 
 
-f = imread('lemon_4.jpg');
+f = imread('lemon_1.jpg');
 f = imcrop(f,[0 0 2750 2000]);
 
 [r, c, num_ch] = size(f);
@@ -24,22 +24,27 @@ ch_max = [154  247  66];
 f_masked = (f_new(:,:,1) >= ch_min(1)) & (f_new(:,:,1) <= ch_max(1)) & ...
            (f_new(:,:,2) >= ch_min(2)) & (f_new(:,:,2) <= ch_max(2)) & ...
            (f_new(:,:,3) >= ch_min(3)) & (f_new(:,:,3) <= ch_max(3));
-       
-f_masked = uint8(255 * f_masked);
-f_new = imgaussfilt(f_new,100.5);
 
-struct_element = offsetstrel('ball',2,0);       
+struct_element = strel('disk',2);       
 f_masked = imdilate(f_masked, struct_element);
-struct_element = offsetstrel('ball',10,0);
+struct_element = strel('disk',10);
 f_masked = imdilate(f_masked, struct_element);
        
        
 subplot(1,3,3);
 imshow(f_masked)
 
+%% Blob Detect  
 
+blob_ana = vision.BlobAnalysis('MinimumBlobArea',500,'MaximumBlobArea', 8000);
+[obj_area, obj_centroid, box_out] = step(blob_ana, f_masked);
 
+%% 
 
+f_shape = insertShape(f_new, 'circle', box_out);
+
+figure
+imshow(f_shape)
 
 
 
